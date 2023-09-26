5506e40d6e6910aa127b44ec0478c5702488f4e1
Johnson <johnson@kmu-informatik.ch>
2023-09-26 15:12:14 +0200

    Refactored Name.java for lombok. Closes #3

--- src/main/java/ch/zhaw/babynames/model/Name.java
+++ src/main/java/ch/zhaw/babynames/model/Name.java
@@ -1,30 +1,14 @@
 package ch.zhaw.babynames.model;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.ToString;
 
+@AllArgsConstructor()
+@ToString()
+@Getter()
 public class Name {
-
-    public Name(String name, String gender, Integer amount) {
-        this.name = name;
-        this.gender = gender;
-        this.amount = amount;
-    }
-
-    @Getter
     private String name;
-
-    @Getter
     private String gender;
-
-    @Getter
     private Integer amount;
-
-    @Override
-    public String toString() {
-        return "Name{" +
-                "name='" + name + '\'' +
-                ", gender='" + gender + '\'' +
-                ", amount=" + amount +
-                '}';
-    }
 }
